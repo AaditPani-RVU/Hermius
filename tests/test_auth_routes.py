73ebcbaa3726e354997d0936b989d8096cfbcbca
AaditPani-RVU <aaditp.btech23@rvu.edu.in>
2025-04-21 10:30:12 +0530

    Random testing

--- tests/test_auth_routes.py
+++ tests/test_auth_routes.py
@@ -46,8 +46,8 @@ def test_signup_login_logout(client):
     response = client.post("/signup", data={
         "username": random_username,
         "email": f"{random_username}@example.com",  # Ensure unique email as well
-        "password": "Test@1234",
-        "confirm_password": "Test@1234"
+        "password": "Test@1235",
+        "confirm_password": "Test@1235"
     }, follow_redirects=True)
 
     # Check if the response was a redirect (status code 200 after redirection)
